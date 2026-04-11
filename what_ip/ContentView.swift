05a5ea3d3828df88f40134c1ef3b27d59a99c8a0
Jesús Chapman <jesuschapman@icloud.com>
2026-04-11 09:24:48 -0500

    Initial Commit

---
+++ what_ip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  what_ip
+//
+//  Created by Jesús David Chapman Vélez on 11/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ what_ip/what_ipApp.swift
@@ -0,0 +1,17 @@
+//
+//  what_ipApp.swift
+//  what_ip
+//
+//  Created by Jesús David Chapman Vélez on 11/04/26.
+//
+
+import SwiftUI
+
+@main
+struct what_ipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
